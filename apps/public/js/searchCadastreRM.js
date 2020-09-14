d03f1643ba908925bc186b0f09f9a34350bc818a
THOMAS Cyril <cy.thomas@mutu.local>
2020-09-14 12:33:41 +0200

    recherche parcelle ajout de controles

--- apps/public/js/searchCadastreRM.js
+++ apps/public/js/searchCadastreRM.js
@@ -80,115 +80,126 @@ var searchCadastreRM = (function () {
             $('.sectionsList').append('<option></option>');
             $('.parcellesList').append('<option></option>');
             // Liste sections
-            $.getJSON(baseUrl_cadastre + 'communes/'+ codeCom +'/sections', function(dataApiJson) {
-                //var htmlContent = '<option value="-1" disabled selected> code section</option>';
-                var htmlContent = '';
-                dataApiJson.forEach(function (data) {
-                    htmlContent += '<option value="'+ data.idSect +'">'+ data.codSect +'</option>'
-                });
-                $('#section').html(htmlContent);
-                $('#section').select2({
-                  placeholder: "section",
-                  allowClear: true,
-                  dropdownAutoWidth: true,
-                  width: '75px',
-                });
-                $(".sectionsList").prop("disabled", false);
-                $(".parcellesList").prop("disabled", true);
-                $(".sectionsList").val("-1").trigger('change');
-            });
+            if (typeof codeCom !== 'undefined' && codeCom !== null && codeCom.trim() !== '') {
+                $.getJSON(baseUrl_cadastre + 'communes/'+ codeCom +'/sections', function(dataApiJson) {
+                  //var htmlContent = '<option value="-1" disabled selected> code section</option>';
+                  var htmlContent = '';
+                  dataApiJson.forEach(function (data) {
+                      htmlContent += '<option value="'+ data.idSect +'">'+ data.codSect +'</option>'
+                  });
+                  $('#section').html(htmlContent);
+                  $('#section').select2({
+                    placeholder: "section",
+                    allowClear: true,
+                    dropdownAutoWidth: true,
+                    width: '75px',
+                  });
+                  $(".sectionsList").prop("disabled", false);
+                  $(".parcellesList").prop("disabled", true);
+                  $(".sectionsList").val("-1").trigger('change');
+              });
+            }
         });
 
         $(document).on('change','#section', function (e) {
-            var codeSection = e.currentTarget.selectedOptions[0].value;
+          var codeSection = '';
+            if (typeof e.currentTarget.selectedOptions[0] !== 'undefined') {
+              codeSection = e.currentTarget.selectedOptions[0].value;
+            }
             $('.parcellesList').val('').trigger('change');
-            $.getJSON(baseUrl_cadastre + 'sections/'+ codeSection +'/parcelles', function(dataApiJson) {
-                //var htmlContent = '<option value="-1" disabled selected> code parcelle</option>';
-                var htmlContent = '';
-                dataApiJson.forEach(function (data) {
-                    htmlContent += '<option value="'+ data.idParc +'">'+ data.numero +'</option>'
-                });
-                $('#parcelle').html(htmlContent);
-                $('#parcelle').select2({
-                  placeholder: "parcelle",
-                  allowClear: true,
-                  dropdownAutoWidth: true,
-                  width: '75px',
-                });
-                $(".parcellesList").prop("disabled", false);
-                $(".parcellesList").val("-1").trigger('change');
-            });
+            if (typeof codeSection !== 'undefined' && codeSection !== null && codeSection.trim() !== '') {
+              $.getJSON(baseUrl_cadastre + 'sections/'+ codeSection +'/parcelles', function(dataApiJson) {
+                  //var htmlContent = '<option value="-1" disabled selected> code parcelle</option>';
+                  var htmlContent = '';
+                  dataApiJson.forEach(function (data) {
+                      htmlContent += '<option value="'+ data.idParc +'">'+ data.numero +'</option>'
+                  });
+                  $('#parcelle').html(htmlContent);
+                  $('#parcelle').select2({
+                    placeholder: "parcelle",
+                    allowClear: true,
+                    dropdownAutoWidth: true,
+                    width: '75px',
+                  });
+                  $(".parcellesList").prop("disabled", false);
+                  $(".parcellesList").val("-1").trigger('change');
+              });
+            }
         });
 
         $(document).on('change','#parcelle', function (e) {
-            var codeParcelle = e.currentTarget.selectedOptions[0].value;
-            var styles = [
-              new ol.style.Style({
-                stroke: new ol.style.Stroke({
-                  color: 'blue',
-                  width: 3
-                }),
-                fill: new ol.style.Fill({
-                  color: 'rgba(0, 0, 255, 0.1)'
+          var codeParcelle = '';
+            if (typeof e.currentTarget.selectedOptions[0] !== 'undefined') {
+              codeParcelle = e.currentTarget.selectedOptions[0].value;
+            }
+              var styles = [
+                new ol.style.Style({
+                  stroke: new ol.style.Stroke({
+                    color: 'blue',
+                    width: 3
+                  }),
+                  fill: new ol.style.Fill({
+                    color: 'rgba(0, 0, 255, 0.1)'
+                  })
                 })
-              })
-            ];
+              ];
+             
+              if (typeof codeParcelle !== 'undefined' && codeParcelle !== null && codeParcelle.trim() !== '') {
+                $.getJSON(baseUrl_cadastre + 'epsg:3948/parcelles/' + codeParcelle, function(dataApiJson) {
+                  var geomNewProj = [];
+                  dataApiJson.coordonnees.geometry.coordinates[0][0].forEach(function (coordinates) {
+                      geomNewProj.push( proj4('EPSG:3948', rmTools.getProjection(), coordinates) );
+                  });
+                  var geojsonObject = {
+                    'type': 'FeatureCollection',
+                    'crs': {
+                      'type': 'name',
+                      'properties': {
+                        'name': 'EPSG:3857'
+                      }
+                    },
+                    'features': [{
+                      'type': 'Feature',
+                      'geometry': {
+                        'type': 'Polygon',
+                        'coordinates': [geomNewProj]
+                      }
+                    } ]
+                  };
+                  var source = new ol.source.Vector({
+                    features: (new ol.format.GeoJSON()).readFeatures(geojsonObject)
+                  });
+                  var layerCadatsreFound = false; 
+                  mviewer.getMap().getLayers().array_.forEach(function (lay) {
+                    if (lay.className_ === 'cadastreLayer' ) {
+                      layerCadatsreFound = true;
+                      lay.setSource(source);
+                      layerExtent = lay.getExtent();
+                      selectedParcelLayer = lay;
+                    }
+                  });
+                  if (!layerCadatsreFound) {
+                    var layer = new ol.layer.Vector({
+                      source: source,
+                      className: 'cadastreLayer',
+                      style: styles,
+                      zIndex: 0
+                    });
+                    mviewer.getMap().addLayer(layer);
+                    selectedParcelLayer = layer;
+                  }
+                  var geometryCenter = getCenterGeometry(geomNewProj);
+                  mviewer.getMap().getView().setCenter(geometryCenter);
+                  mviewer.getMap().getView().setZoom(17);
     
-            $.getJSON(baseUrl_cadastre + 'epsg:3948/parcelles/' + codeParcelle, function(dataApiJson) {
-              var geomNewProj = [];
-              dataApiJson.coordonnees.geometry.coordinates[0][0].forEach(function (coordinates) {
-                  geomNewProj.push( proj4('EPSG:3948', rmTools.getProjection(), coordinates) );
-              });
-              var geojsonObject = {
-                'type': 'FeatureCollection',
-                'crs': {
-                  'type': 'name',
-                  'properties': {
-                    'name': 'EPSG:3857'
-                  }
-                },
-                'features': [{
-                  'type': 'Feature',
-                  'geometry': {
-                    'type': 'Polygon',
-                    'coordinates': [geomNewProj]
-                  }
-                } ]
-              };
-              var source = new ol.source.Vector({
-                features: (new ol.format.GeoJSON()).readFeatures(geojsonObject)
-              });
-              var layerCadatsreFound = false; 
-              mviewer.getMap().getLayers().array_.forEach(function (lay) {
-                if (lay.className_ === 'cadastreLayer' ) {
-                  layerCadatsreFound = true;
-                  lay.setSource(source);
-                  layerExtent = lay.getExtent();
-                  selectedParcelLayer = lay;
-                }
-              });
-              if (!layerCadatsreFound) {
-                var layer = new ol.layer.Vector({
-                  source: source,
-                  className: 'cadastreLayer',
-                  style: styles,
-                  zIndex: 0
+                  var e = {
+                    coordinate:geometryCenter,
+                    pixel: mviewer.getMap().getPixelFromCoordinate(mviewer.getMap().getView().getCenter())
+                  };
+                  info.queryMap(e);
+  
                 });
-                mviewer.getMap().addLayer(layer);
-                selectedParcelLayer = layer;
               }
-              var geometryCenter = getCenterGeometry(geomNewProj);
-              mviewer.getMap().getView().setCenter(geometryCenter);
-              mviewer.getMap().getView().setZoom(17);
-
-              var e = {
-                coordinate:geometryCenter,
-                pixel: mviewer.getMap().getPixelFromCoordinate(mviewer.getMap().getView().getCenter())
-              };
-              info.queryMap(e);
-
-            });
-    
         });
 
         /*$(document).on('click','#cleanParcel', function (e) {
